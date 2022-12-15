8c8c5fbfbb432ae4101ededfd27dc45c0c695ed2
Diego Medeiros <diego.medeiros@oceanbrasil.com>
2022-12-15 18:16:16 -0400

    Checkpoint aula 1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,34 @@
 import './App.css'
 
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
+import Card from "./components/Card/Card";
+
 function App() {
   return (
     <div className="App">
- 
-      <div className="content">
+      <Header />
+
+      <div className="content container mb-3 mt-3">
+        <div className="row">
+          <div className="col-4">
+            <Card />
+          </div>
+          <div className="col-4">
+            <Card />
+          </div>
+          <div className="col-4">
+            <Card />
+          </div>
+          <div className="col-4">
+            <Card />
+          </div>
+        </div>
       </div>
 
+      <Footer />
     </div>
-  )
+  );
 }
 
 export default App
